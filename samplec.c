d7952cb00771c0b98d1ba684755cbcd3601ed941
Sidharth <sidharthks60@gmail.com>
2021-10-06 14:57:22 +0530

    First copy of my file

---
+++ samplec.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+ void addtwo()
+ {
+   int n1, n2;
+   printf("Enter  two intergers ");
+   scanf("%d %d ", &n1, &n2);
+
+   printf("Sum of two numbers are %d ", n1+2);
+ }
+
+ void main()
+  {
+    
+  }
